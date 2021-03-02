32692dce75d9692df7002739c9083b24dc0c7ce5
Lou Lefrançois <lefrancois.lou@gmail.com>
2021-03-02 23:52:44 +0100

    13

--- js/script_projets.js
+++ js/script_projets.js
@@ -1,4 +1,24 @@
 $("#header").load("header.html");
 
-var gridDeuxHeight = $("#premierContainer #FR").outerHeight(true) + "px";
-$("#secondContainer").css("margin-top", gridDeuxHeight);
+$(document).ready(function () {
+    if ($(window).width() >= 1090) {
+
+        var gridDeuxHeight = $("#premierContainer #FR").outerHeight(true) + "px";
+        $("#secondContainer").css("margin-top", gridDeuxHeight);
+
+    }
+});
+
+$(document).ready(function () {
+    if ($(window).width() <= 1089) {
+        
+        var descriptionProjetMarginTop = $("#premierContainer .grid_8").outerHeight(true) + 80 ;
+        
+        $("#description_projet").css("margin-top", descriptionProjetMarginTop);
+        
+        
+        var secondContainerMarginTop = $("#description_projet").outerHeight(true) + $("#premierContainer .grid_8").outerHeight(true) + "px";
+        $("#secondContainer").css("margin-top", secondContainerMarginTop);
+        
+    }
+});
