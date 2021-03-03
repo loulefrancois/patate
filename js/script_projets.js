64bea8f7a2c1847f0413bf8991893f494912869a
Lou Lefrançois <lefrancois.lou@gmail.com>
2021-03-03 11:41:38 +0100

    13

--- js/script_projets.js
+++ js/script_projets.js
@@ -1,24 +1,28 @@
-$("#header").load("header.html");
+$("#header").load("../header.html");
 
-$(document).ready(function () {
-    if ($(window).width() >= 1090) {
 
+
+if ($(window).width() >= 1090) {
+
+    $(document).ready(marginTopSecondContainer);
+    $(window).on('resize', marginTopSecondContainer);
+
+    function marginTopSecondContainer() {
         var gridDeuxHeight = $("#premierContainer #FR").outerHeight(true) + "px";
         $("#secondContainer").css("margin-top", gridDeuxHeight);
-
     }
-});
+};
 
 $(document).ready(function () {
     if ($(window).width() <= 1089) {
-        
-        var descriptionProjetMarginTop = $("#premierContainer .grid_8").outerHeight(true) + 80 ;
-        
+
+        var descriptionProjetMarginTop = $("#premierContainer .grid_8").outerHeight(true) + 120;
+
         $("#description_projet").css("margin-top", descriptionProjetMarginTop);
-        
-        
-        var secondContainerMarginTop = $("#description_projet").outerHeight(true) + $("#premierContainer .grid_8").outerHeight(true) + "px";
+
+
+        var secondContainerMarginTop = $("#description_projet").outerHeight(true) + $("#premierContainer .grid_8").outerHeight(true) + 80 + "px";
         $("#secondContainer").css("margin-top", secondContainerMarginTop);
-        
+
     }
 });
