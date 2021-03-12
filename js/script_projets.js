bd77e8017d0c11877c0573ef616adf9ff4d9ff4d
Lou Lefrançois <lefrancois.lou@gmail.com>
2021-03-12 18:53:42 +0100

    ...

--- js/script_projets.js
+++ js/script_projets.js
@@ -2,16 +2,52 @@ $("#header").load("../header.html");
 
 
 
-if ($(window).width() >= 1090) {
+$(document).ready(function () {
+    if ($(window).width() >= 1090) {
 
-    $(document).ready(marginTopSecondContainer);
-    $(window).on('resize', marginTopSecondContainer);
+        // Marges page projet 2nd container
 
-    function marginTopSecondContainer() {
-        var gridDeuxHeight = $("#premierContainer #FR").outerHeight(true) + "px";
-        $("#secondContainer").css("margin-top", gridDeuxHeight);
-    }
-};
+        $(document).ready(marginTopSecondContainer);
+        $(window).on('resize', marginTopSecondContainer);
+
+        function marginTopSecondContainer() {
+            var gridDeuxHeight = $("#premierContainer #FR").outerHeight(true) + "px";
+            $("#secondContainer").css("margin-top", gridDeuxHeight);
+        }
+
+        // Image index
+        $("h1").mouseover(function () {
+            $(this).parent().find("img").css({
+                "opacity": "1",
+                "transition": "opacity 0.2s ease-in-out"
+            });
+            $("img").not($(this).parent().find("img")).css({
+                "opacity": "0"
+            });
+        });
+
+        $("h1").mouseout(function () {
+            $(this).parent().find("img").css({
+                "opacity": "0"
+            });
+
+        });
+
+        $(document).mousemove(function (e) {
+
+            var imagePositionX = e.pageX + 15;
+            var imagePositionY = e.pageY + 15;
+
+            $("img").css({
+                "left": imagePositionX,
+                "top": imagePositionY
+            });
+        });
+
+    };
+
+});
+
 
 $(document).ready(function () {
     if ($(window).width() <= 1089) {
@@ -19,10 +55,12 @@ $(document).ready(function () {
         var descriptionProjetMarginTop = $("#premierContainer .grid_8").outerHeight(true) + 120;
 
         $("#description_projet").css("margin-top", descriptionProjetMarginTop);
-
-
-        var secondContainerMarginTop = $("#description_projet").outerHeight(true) + $("#premierContainer .grid_8").outerHeight(true) + 80 + "px";
+        $("#premierContainer .grid_8").outerHeight(true) + 80 + "px";
         $("#secondContainer").css("margin-top", secondContainerMarginTop);
+
 
     }
 });
+
+
+// À Propos
